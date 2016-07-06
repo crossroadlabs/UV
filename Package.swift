ff49749a8b8cf4ae7d2f64585da200d6d4590154
Yehor Popovych <y.popovych@crossroadlabs.xyz>
2016-07-06 15:32:44 +0300

    Added XCTest3 to Package.swift

--- Package.swift
+++ Package.swift
@@ -21,6 +21,7 @@ let package = Package(
     targets: [Target(name: "UV")],
     dependencies: [
         .Package(url: "https://github.com/reactive-swift/CUV.git", majorVersion: 0, minor: 1),
-        .Package(url: "https://github.com/crossroadlabs/Boilerplate.git", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/crossroadlabs/Boilerplate.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/crossroadlabs/XCTest3.git", majorVersion: 0, minor: 2)
     ]
 )
