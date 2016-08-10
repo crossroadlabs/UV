98ba87e5634e1c5783d1ccd9dd1d4817f4469ec7
Yehor Popovych <y.popovych@crossroadlabs.xyz>
2016-08-10 15:55:32 +0300

    Boilerplate version update

--- Package.swift
+++ Package.swift
@@ -21,6 +21,6 @@ let package = Package(
     targets: [Target(name: "UV")],
     dependencies: [
         .Package(url: "https://github.com/reactive-swift/CUV.git", majorVersion: 0, minor: 1),
-        .Package(url: "https://github.com/crossroadlabs/Boilerplate.git", Version(1, 0, 0, prereleaseIdentifiers: ["alpha", "1"])),
+        .Package(url: "https://github.com/crossroadlabs/Boilerplate.git", Version(1, 0, 0, prereleaseIdentifiers: ["alpha", "2"])),
     ]
 )
